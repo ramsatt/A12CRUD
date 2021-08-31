09ccaf1bad748d4b27b2eb4e1c5896b8cd680d27
Sathish Kumar <ramsatt@gmail.com>
2021-08-31 21:51:30 +0530

    Layout Creation

--- src/app/crud/crud.module.ts
+++ src/app/crud/crud.module.ts
@@ -5,6 +5,7 @@ import { CRUDRoutingModule } from './crud-routing.module';
 import { CreateProductComponent } from './create-product/create-product.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import {AgGridModule} from "ag-grid-angular";
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   ],
   imports: [
     CommonModule,
-    CRUDRoutingModule
+    CRUDRoutingModule,
+    AgGridModule.withComponents([ProductListComponent])
   ]
 })
 export class CRUDModule { }
--- src/app/crud/product-list/product-list.component.ts
+++ src/app/crud/product-list/product-list.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  columnDefs = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price'}
+  ];
 
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
   constructor() { }
 
   ngOnInit(): void {

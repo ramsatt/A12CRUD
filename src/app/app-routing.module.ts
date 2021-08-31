409f43555c1768f5d5aba3f0c8229cb5eb47143c
Sathish Kumar <ramsatt@gmail.com>
2021-08-31 21:24:27 +0530

    Router Config - Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'crud', pathMatch: 'full'},
+  {path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CRUDModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
